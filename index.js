6404284a8c579844fe4fcf6a48bb34c42bcdcb3f
Rizky Kang Turu <88314302+rizxyu@users.noreply.github.com>
2024-01-07 12:15:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+/*DONT SELL THIS CODE BASTARD!
+sialan lu anak jb awas aja kalo dijual
+tinggal pake module gw napa!
+Credit
+api fabdl
+-WIZZ TEAM (RIZXYU, RESTU, FAUZAN)
+*/
+import axios from "axios";
 
+// info code
+const getInfo = async (query: string): Promise<string> => {
+  try {
+    const { data } = await axios.get(`https://api.fabdl.com/apple-music/get?url=${encodeURIComponent(query)}`);
+    return data; // Return the download URL
+  } catch (error) {
+    throw new Error("Applemusic Function Error");
+  }
+};
+
+// download code
+const download = async (query: string): Promise<string> => {
+  try {
+    const { data: { result: { id, gid } } } = await axios.get(`https://api.fabdl.com/apple-music/get?url=${encodeURIComponent(query)}`);
+    const { data: { result: { tid } } } = await axios.get(`https://api.fabdl.com/apple-music/mp3-convert-task/${gid}/${id}`);
+    const url = `https://api.fabdl.com/apple-music/download-mp3/${tid}`;
+    return url; // Return the download URL
+  } catch (error) {
+    throw new Error("Applemusic Function Error");
+  }
+};
+
+export default { getInfo, download};
